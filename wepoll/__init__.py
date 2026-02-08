7ad06376b16f94b58b7c7ac30ea825164cb27323
Vizonex <114684698+Vizonex@users.noreply.github.com>
2026-02-08 14:39:53 -0600

    Update version to 0.3.3 and maintain imports

--- wepoll/__init__.py
+++ wepoll/__init__.py
@@ -15,7 +15,7 @@ from .loop import WepollEventLoop
 from .selector import EpollSelector
 
 __author__ = "Vizonex"
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 __all__ = (
     "EPOLLERR",
     "EPOLLHUP",
@@ -33,5 +33,6 @@ __all__ = (
     "__version__",
     "epoll",
 )
+
 
 
